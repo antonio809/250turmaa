b641b050534e9e5a65bcdd48ddaa2880752fc5c0
antonio809 <antoniojorgeqk@gmail.com>
2024-10-23 10:32:07 -0300

    Aula de hoje adicionada.

--- LP_I/programas/Aula16101.java
+++ LP_I/programas/Aula16101.java
@@ -8,5 +8,29 @@ public class Aula16101 {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Informe 7 números:");
+        double numeros[] = new double[7];
+
+        for(int i = 0; i < 7; i++){
+            System.out.print((i + 1) + "o. número: ");
+            numeros[i] = entrada.nextDouble();
+        }
+
+        System.out.print("Número buscado: ");
+        double busca = entrada.nextDouble();
+
+        boolean naoTemBusca = true;
+
+        for(int i = 0; i < 7; i++){
+            if(busca == numeros[i]){
+                System.out.println("Número existe");
+                System.out.println("Posição: " + (i + 1));
+                naoTemBusca = false;
+            }
+        }
+        if(naoTemBusca){
+            System.out.println("Não existe o número que você buscou.");
+        }
+
+        System.out.println("Fim do programa\nBye bye.");
     }
 }
---
+++ LP_I/programas/Aula2310.java
@@ -0,0 +1,30 @@
+package programas;
+import java.util.Scanner;
+
+public class Aula2310 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double matriz[][] = new double[3][3];
+
+        // Linha 0
+        matriz[0][0] = 120;
+        matriz[0][1] = 232;
+        matriz[0][2] = 201;
+        
+        // linha 1
+        matriz[1][0] = 129;
+        matriz[1][1] = 111;
+        matriz[1][2] = 187;
+
+        // linha 2
+        matriz[2][0] = 128;
+        matriz[2][1] = 247;
+        matriz[2][2] = 123;
+
+
+
+
+        entrada.close();
+    }
+}
