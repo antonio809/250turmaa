5035130f64047587fb5cfeea73d20ba985564cfb
antonio <164699899+antonio809@users.noreply.github.com>
2025-02-20 10:48:31 -0300

    Add files via upload

---
+++ classes/ContaBancaria.java
@@ -0,0 +1,12 @@
+package classes;
+
+
+public class ContaBancaria {
+    
+    public int numero;
+    public String titular;
+    public double saldo;
+    public String tipoConta;
+    public int agencia;
+    public String banco;
+}
---
+++ classes/Pessoa.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Pessoa {
+    
+    public String nome;
+    public int idade;
+    public int cpf;
+    public String endereco;
+    public int telefone;
+}
